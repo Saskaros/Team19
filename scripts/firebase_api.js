42bb459a003ea89cfc09c468084cfbf7e693da48
Leanne Holmes <lholmes24@my.bcit.ca>
2021-03-18 16:50:40 -0700

    Updated index.html, added login page

---
+++ scripts/firebase_api.js
@@ -0,0 +1,12 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCrRgYXNRD1vtIPfI2SwAGT0d0GUleayYg",
+    authDomain: "comp1800-team19.firebaseapp.com",
+    projectId: "comp1800-team19",
+    storageBucket: "comp1800-team19.appspot.com",
+    messagingSenderId: "10374778089",
+    appId: "1:10374778089:web:54394fc2561ca6d1b03478"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
